9ed45c428a119414f22a746c09879d44e787343b
RickaPrincy <rckprincy@gmail.com>
2024-03-13 10:48:14 +0300

    chore: init project with spring initializer

---
+++ src/main/java/com/hei/wallet/wallety/WallEtyApplication.java
@@ -0,0 +1,13 @@
+package com.hei.wallet.wallety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallEtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallEtyApplication.class, args);
+    }
+
+}
